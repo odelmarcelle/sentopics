c13e713ac95984e23f9c2a6680fc460b4285539e
odlmarce <delmarcelle.olivier@gmail.com>
2022-03-22 01:54:52 +0100

    conversions from topicmodels, lda and seededlda

--- R/conversions.R
+++ R/conversions.R
@@ -4,26 +4,66 @@
 #' Conversions from other packages to LDA
 #'
 #' @description These functions converts estimated models from other topic
-#'   modeling packages to the format used by sentopics.
+#'   modeling packages to the format used by \pkg{sentopics}.
 #'
-#' @param x an estimated topic model from the \pkg{stm} package.
-#' @param docs for the conversion from \pkg{stm}, the documents used to create
-#'   the object.
+#' @param x an estimated topic model from \pkg{stm}, \pkg{topicmodels} or
+#'   \pkg{seededlda}.
+#' @param list the list containing an estimated model from \pkg{lda}.
+#' @param docs for some objects, the documents used to initialize the model.
+#' @param alpha for \pkg{lda} models, the document-topic mixture hyperparameter.
+#'   If missing, the hyperparameter will be set to `50/K`.
+#' @param eta for \pkg{lda} models, the topic-word mixture hyperparameter. Other
+#'   packages refer to this hypeparameter as *beta*. If missing, the
+#'   hyperparameter will be set to `0.01`.
 #' @param ... arguments passed to other methods.
 #'
-#' @details The models initially estimated through variational inference suffer
-#'   from restricted support. Hence, it is not possible to rename topics or to
-#'   merge them.
+#' @details Some models do not store the topic assignment of each word (for
+#'   example, estimated through variational inference). For these, the
+#'   conversion is limited and some functionalities of \pkg{sentopics} will be
+#'   disabled. The list of affected functions is subject to change and currently
+#'   includes [grow()], [mergeTopics()] and [rJST.LDA()].
+#'
+#'   Since models from the \pkg{lda} package are simply lists of outputs, the
+#'   function `as.LDA_lda()` is not related to the other methods and should be
+#'   applied directly on lists containing a model.
+#'
+#' @return A S3 list of class `LDA`, as if it was created and estimated using
+#'   [LDA()] and [grow()].
 #'
 #' @rdname as.LDA
 #' @export
 #' @examples
 #' \donttest{
-#' library(stm)
+#' ## stm
+#' library("stm")
 #' stm <- stm(poliblog5k.docs, poliblog5k.voc, K=25,
 #'            prevalence=~rating, data=poliblog5k.meta,
-#'            max.em.its=2, init.type="Spectral") 
+#'            max.em.its=2, init.type="Spectral")
 #' as.LDA(stm, docs = poliblog5k.docs)
+#'
+#' ## lda
+#' library("lda")
+#' data("cora.documents")
+#' data("cora.vocab")
+#' lda <- lda.collapsed.gibbs.sampler(cora.documents,
+#'                                    5, ## Num clusters
+#'                                    cora.vocab,
+#'                                    100, ## Num iterations
+#'                                    0.1,
+#'                                    0.1)
+#' LDA <- as.LDA_lda(lda, cora.documents, alpha = .1, eta = .1)
+#'
+#' ## topicmodels
+#' data("AssociatedPress", package = "topicmodels")
+#' lda <- topicmodels::LDA(AssociatedPress[1:20,],
+#'                         control = list(alpha = 0.1), k = 2)
+#' LDA <- as.LDA(lda, AssociatedPress[1:20,])
+#'
+#' ## seededlda
+#' library("seededlda")
+#' lda <- textmodel_lda(dfm(ECB_press_conferences_tokens),
+#'                      k = 6, max_iter = 100)
+#' LDA <- as.LDA(lda)
 #' }
 as.LDA <- function(x, ...) {
   UseMethod("as.LDA")
@@ -118,7 +158,8 @@ as.LDA.STM <- function(x, docs, ...) {
     x = lapply(docs, function(doc) rep(doc[1, ], times = doc[2, ])),
     types = x$vocab,
     docvars = make_docvars(length(docs)))
-  stopifnot(isTRUE(all.equal(unname(lengths(tokens)), doc.length)))
+  stopifnot(isTRUE(all.equal(unname(lengths(cleanPadding(tokens))),
+                             doc.length)))
 
   vocabulary <- makeVocabulary(tokens, NULL, 1L)
 
@@ -142,6 +183,208 @@ as.LDA.STM <- function(x, docs, ...) {
 }
 
 
+#' @rdname as.LDA
+#' @export
+as.LDA.LDA_Gibbs <- function(x, docs, ...) {
+  K <- x@k
+  alpha <- x@alpha
+  beta <- x@beta
+  beta[] <- x@control@delta
+  
+  tokens <- quanteda::as.tokens(quanteda::as.dfm(docs))
+  vocabulary <- makeVocabulary(tokens, NULL, 1L)
+  
+  za <- split(x@z, rep(seq_len(length(tokens)),
+                       times = lengths(cleanPadding(tokens))))
+  
+  LDA <- structure(list(
+    tokens = vocabulary$toks,
+    vocabulary = vocabulary$vocabulary,
+    K = K,
+    alpha = as.matrix(rep(alpha, K)),
+    beta = beta,
+    it = x@iter,
+    theta = x@gamma,
+    phi = exp(t(x@beta)),
+    za = za,
+    logLikelihood = NULL
+  ), class = c("LDA", "sentopicmodel"), reversed = TRUE, Sdim = "L2")
+  LDA <- grow(LDA, 0, displayProgress = FALSE)
+  
+  LDA
+}
+
+#' @rdname as.LDA
+#' @export
+as.LDA.LDA_VEM <- function(x, docs, ...) {
+  K <- x@k
+  alpha <- x@alpha
+  alpha <- rep(alpha, K)
+  
+  dfm <- quanteda::as.dfm(docs)
+  tokens <- quanteda::as.tokens(dfm)
+  dfm <- as.matrix(dfm)
+  vocabulary <- makeVocabulary(tokens, NULL, 1L)
+  
+  doc.length <- lengths(tokens, use.names = FALSE)
+  zd <- rebuild_L1d_from_posterior(lengths(tokens), x@gamma, alpha)
+  zd <- t(zd)
+  
+  ## adjust zd to integer
+  diff <- doc.length - as.integer(colSums(round(zd)))
+  for (i in which(diff != 0)) {
+    while (diff[i] != 0L) {
+      possible <- zd[, i] > (-diff[i] - .5)
+      idx <- which.min( (zd[possible, i] - round(zd[possible, i])*diff[i] ))
+      zd[which(possible)[idx], i] <- zd[which(possible)[idx], i] + 1*sign(diff[i])
+      diff[i] <- as.integer(doc.length[i] - sum(round(zd[, i])))
+    }
+  }
+  zd <- round(zd)
+  storage.mode(zd) <- "integer"
+  stopifnot(isTRUE(all.equal(colSums(zd), doc.length)))
+  
+  phi <- t(exp(x@beta))
+  beta <- x@beta
+  beta[] <- .Machine$double.eps
+  
+  ## adjust zw to integer
+  zw <- rebuild_zw_from_posterior2(zd, phi, beta)
+  diff <- as.integer(colSums(dfm) - colSums(round(zw)))
+  for (i in which(diff != 0)) {
+    while (diff[i] != 0L) {
+      possible <- (zw[, i] > (-diff[i] - .5))
+      idx <- which.min( (zw[possible, i] - round(zw[possible, i])*diff[i] ))
+      zw[which(possible)[idx], i] <- zw[which(possible)[idx], i] + 1*sign(diff[i])
+      diff[i] <- as.integer(sum(dfm[, i]) - sum(round(zw[, i])))
+    }
+  }
+  zw <- round(zw)
+  diff2 <- as.integer(rowSums(zd) - rowSums(round(zw)))
+  ## quickly adjust second dimension
+  while (any(diff2 < 0)) {
+    count <- 1L;
+    while (TRUE) {
+      non_zero <- length(zw[diff2 < 0][zw[diff2 < 0] > count])
+      deduct <- ceiling(abs(min(diff2))/non_zero)
+      if (deduct <= count) break
+      else count <- count + 1
+    }
+    zw[diff2 < 0][zw[diff2 < 0] > count] <- zw[diff2 < 0][zw[diff2 < 0] > count] - deduct 
+    diff2 <- as.integer(rowSums(zd) - rowSums(round(zw)))
+  }
+  diff <- as.integer(colSums(dfm)) - as.integer(colSums(round(zw)))
+  zw <- zw + stats::r2dtable(1, diff2, diff)[[1]]
+  storage.mode(zw) <- "integer"
+  stopifnot(isTRUE(all.equal(colSums(zw), unname(colSums(dfm)))))
+  stopifnot(isTRUE(all.equal(rowSums(zw), rowSums(zd))))
+  
+  LDA <- structure(list(
+    tokens = vocabulary$toks,
+    vocabulary = vocabulary$vocabulary,
+    K = K,
+    alpha = as.matrix(alpha),
+    beta = beta,
+    it = x@iter,
+    zd = zd,
+    zw = zw,
+    theta = x@gamma,
+    phi = phi,
+    logLikelihood = x@logLiks
+  ), class = c("LDA", "sentopicmodel"), reversed = TRUE, Sdim = "L2",
+  approx = TRUE)
+  LDA <- as.LDA(reorder_sentopicmodel(LDA))
+  
+  LDA
+}
+
+#' @rdname as.LDA
+#' @export
+as.LDA.textmodel_lda <- function(x, ...) {
+  
+  labels <- colnames(x$theta)
+  
+  beta <- x$phi
+  beta[] <- x$beta
+  alpha <- rep(x$alpha, x$k)
+  
+  tokens <- as.tokens(x$data)
+  vocabulary <- makeVocabulary(tokens, NULL, 1L)
+  
+  zd <- rebuild_L1d_from_posterior(lengths(cleanPadding(tokens)), x$theta, alpha)
+  zd <- t(zd)
+  zd <- round(zd)
+  storage.mode(zd) <- "integer"
+  
+  zw <- t(as.matrix(x$words))
+  zw <- round(zw)
+  storage.mode(zw) <- "integer"
+  
+  LDA <- structure(list(
+    tokens = vocabulary$toks,
+    vocabulary = vocabulary$vocabulary,
+    K = x$k,
+    alpha = as.matrix(rep(x$alpha, x$k)),
+    beta = beta,
+    it = x$last_iter,
+    theta = x$theta,
+    phi = t(x$phi),
+    zd = zd,
+    zw = zw,
+    logLikelihood = NULL
+  ), class = c("LDA", "sentopicmodel"), reversed = TRUE, Sdim = "L2",
+  approx = TRUE, labels = list(L1 = colnames(x$theta)))
+  LDA <- as.LDA(reorder_sentopicmodel(LDA))
+  
+  LDA
+}
+
+#' @rdname as.LDA
+#' @export
+# @usage as.LDA.lda(list, docs, alpha, eta)
+as.LDA_lda <- function(list, docs, alpha, eta) {
+  zw <- list$topics
+  zd <- list$document_sums
+  vocab <- colnames(zw)
+  
+  K <- nrow(zw)
+  
+  build_tokens <- get("build_tokens", envir = getNamespace("quanteda"))
+  make_docvars <- get("make_docvars", envir = getNamespace("quanteda"))
+  tokens <- build_tokens(
+    x = lapply(docs, function(doc) rep(doc[1, ] + 1L, times = doc[2, ])),
+    types = colnames(zw),
+    docvars = make_docvars(length(docs)))
+  
+  stopifnot(isTRUE(all.equal(unname(lengths(cleanPadding(tokens))),
+                             colSums(zd))))
+  
+  za <- mapply(function(doc, assignments) rep(assignments + 1L, times = doc[2, ]),
+               docs, list$assignments)
+  
+  vocabulary <- makeVocabulary(tokens, NULL, 1L)
+  
+  if (missing(alpha)) alpha <- 50/K
+  if (missing(eta)) eta <- 0.01
+  beta <- zw
+  beta[] <- eta
+  
+  LDA <- structure(list(
+    tokens = vocabulary$toks,
+    vocabulary = vocabulary$vocabulary,
+    K = K,
+    alpha = as.matrix(rep(alpha, K)),
+    beta = beta,
+    it = 1,
+    theta = t(zd),
+    phi = t(zw),
+    za = za,
+    logLikelihood = NULL
+  ), class = c("LDA", "sentopicmodel"), reversed = TRUE, Sdim = "L2")
+  LDA <- grow(LDA, 0, displayProgress = FALSE)
+  
+  LDA
+}
 
 # To sentopicmodel --------------------------------------------------------
 
